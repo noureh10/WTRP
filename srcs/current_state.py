098741375172ac121ca45f8ae3df928a73014ca2
Nour Echaara <58146@etu.he2b.be>
2024-11-03 23:41:43 +0100

    4 - Current state file, working on it

--- srcs/current_state.py
+++ srcs/current_state.py
@@ -5,13 +5,25 @@ INDICATOR_WT_API = "http://localhost:8111/indicators"
 MAP_IMAGE = "http://localhost:8111/map.img"
 
 def get_indicators():
-    response = requests.get(INDICATOR_WT_API)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return None
+	response = requests.get(INDICATOR_WT_API)
+	if response.status_code == 200:
+		return response.json()
+	else:
+		return None
+
+def get_map_img():
+	response = requests.get(MAP_IMAGE)
+	if  response.status_code == 200:
+			return response
+	else:
+		return None
+
+HANGAR_MSG = "Currently in the hangar"
 
 def get_vehicle():
 	data = get_indicators()
-	if data is not None :
+	map = get_map_img()
+	if ((data is not None and map is None)):
+		return (HANGAR_MSG)
+	if ((data is not None and str(data["valid"]) == "True")):
 		return (data["type"])
